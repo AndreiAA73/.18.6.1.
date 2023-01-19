733158c447d989727bafc784e039dbb176349aa0
Andrei <119425365+AndreiAA73@users.noreply.github.com>
2023-01-19 17:52:05 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+VALUES = {
+    "RUB": True,
+    "USD": True,
+    "ETH": True,
+    "BTC": True,
+    "EUR": True,
+    "CNY": True,
+    "AED": True,
+    "THB": True,
+}
+TOKEN = '5767995675:AAF211ugqwF7_ZD4yU64HBu_Gicw9htUqzQ'
+API_KEY = "0b535d9f3fcd08d05fe144034bd6f14960f141fff7cdb01aea625cd1cdd4e98c"
+API = "https://min-api.cryptocompare.com/data/price"
